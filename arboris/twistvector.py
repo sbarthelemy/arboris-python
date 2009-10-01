sbarthelemy/arboris-python

commit 7c20868a8b46809341386f746ccb860c9b16c63e

fix bug in exponential

arboris/twistvector.py
@@ -62,6 +62,7 @@ def exp(tw):
         dsc = 1./6.
     
     R = eye(3) + sc*wx + cc*dot(wx,wx)
-    p = dot(sc*eye(3)+cc*wx+dsc*dot(w,w.T), v)
+    w_3x1 = w.copy().reshape(3,1) #TODO improve efficiency
+    p = dot(sc*eye(3)+cc*wx+dsc*dot(w_3x1,w_3x1.T), v)
     return vstack((hstack((R, p.reshape(3,1))),
                    array([[0., 0., 0., 1.]])))

commit e711bbff44126b5f6ccd183c92a90adb649ee0d3

fix doctest error introduced in 7c20868a8b46809341386f746ccb860c9b16c63e

arboris/twistvector.py
@@ -39,9 +39,9 @@ def exp(tw):
 
     >>> t = array([1., 2., 3., 10., 11., 12.])
     >>> exp(t)
-    array([[ -0.69492056,   0.71352099,   0.08929286,  35.29778399],
-           [ -0.19200697,  -0.30378504,   0.93319235,  38.66708228],
-           [  0.69297817,   0.6313497 ,   0.34810748,  34.99594133],
+    array([[ -0.69492056,   0.71352099,   0.08929286,   2.90756949],
+           [ -0.19200697,  -0.30378504,   0.93319235,  11.86705709],
+           [  0.69297817,   0.6313497 ,   0.34810748,  13.78610544],
            [  0.        ,   0.        ,   0.        ,   1.        ]])
 
     """