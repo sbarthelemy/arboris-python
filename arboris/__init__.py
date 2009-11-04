a68142efa4e2a11d51d16e8ee44990f6b4a4f274
Sébastien Barthélemy <seb@navi.(none)>
2009-11-04 12:40:02 +0100

    make visu_osg and qpcontroller import optional

--- arboris/__init__.py
+++ arboris/__init__.py
@@ -1,4 +1,24 @@
+# coding=utf-8
+"""Check which modules are availlable."""
+
+__author__ = ("Sébastien BARTHÉLEMY <barthelemy@crans.org>")
+
+def optional_modules():
+    """A list of optional modules which are available."""
+    mods = []
+    try:
+        import visu_osg
+        mods.append('visu_osg')
+    except ImportError:
+        pass
+    try:
+        import qpcontroller
+        mods.append('qpcontroller')
+    except ImportError:
+        pass
+    return mods
+
 __all__ = ['adjointmatrix', 'collisions', 'constraints', 'controllers',
            'core', 'homogeneousmatrix', 'joints', 'massmatrix',
-           'observers', 'qpcontroller', 'rigidmotion', 'robots',
-           'shapes', 'twistvector', 'visu_osg']
+           'observers', 'rigidmotion', 'robots', 'shapes', 'twistvector']
+__all__.extend(optional_modules())
