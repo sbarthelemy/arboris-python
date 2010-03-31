3fa860ad4d02711cae4f96a7bd4063cac7802be8
Sébastien Barthélémy <barthelemy@crans.org>
2010-04-01 00:17:06 +0200

    removed redundancy between simplearm and massmatrix
    WIP

--- arboris/robots/simplearm.py
+++ arboris/robots/simplearm.py
@@ -13,26 +13,11 @@ __author__ = (u"Sébastien BARTHÉLEMY <sebastien.barthelemy@gmail.com>")
 from arboris.core import World, Body, SubFrame
 import numpy as np
 import arboris.homogeneousmatrix as Hg
+import arboris.massmatrix
 from arboris.joints import RzJoint
 
-def transport_mass_matrix(mass, H):
-    """Transport (express) the mass matrix into another frame."""
-    Ad = Hg.iadjoint(H)
-    return np.dot(
-        Ad.transpose(),
-        np.dot(mass, Ad))
-        
-def mass_parallelepiped(m, lengths):
-    """The mass matrix of an homogeneous parallelepiped."""
-    (a, b, c) = lengths
-    return np.diag((
-        m/12.*(b**2+c**2 ), 
-        m/12.*(a**2+c**2), 
-        m/12.*(a**2+b**2),
-        m, m, m))
-
-def add_simplearm(world, name='', lengths=(0.5, 0.4 , 0.2),
-                masses=(1.0, 0.8, 0.2)):
+def add_simplearm(world, name='', lengths=(0.5 ,0.4 , 0.2),
+                  masses=(1.0, 0.8, 0.2)):
     """Build a  planar 3-R robot.
 
     TODO: make use of the ``name`` input argument to prefix bodies and joints names
@@ -56,26 +41,25 @@ def add_simplearm(world, name='', lengths=(0.5, 0.4 , 0.2),
     # create bodies
     arm = Body(
         name=name+'Arm',
-        mass=transport_mass_matrix(
-            mass_parallelepiped(
-                arm_mass, 
-                (arm_length/10, arm_length, arm_length/10)),
-                Hg.transl(0, arm_length/2, 0)))
+        mass=arboris.massmatrix.transport(
+            arboris.massmatrix.box(
+                (arm_length/20., arm_length/2., arm_length/20.),
+                arm_mass),
+            Hg.transl(0., -arm_length/2., 0.)))
     forearm = Body(
         name=name+'ForeArm',
-        mass=transport_mass_matrix(
-            mass_parallelepiped(
-                forearm_mass, 
-                (forearm_length/10, forearm_length, forearm_length/10)),
-                Hg.transl(0, forearm_length/2, 0)))
+        mass=arboris.massmatrix.transport(
+            arboris.massmatrix.box(
+                (forearm_length/20., forearm_length/2., forearm_length/20.),
+                forearm_mass),
+            Hg.transl(0., -forearm_length/2., 0.)))
     hand = Body(
         name=name+'Hand',
-        mass=transport_mass_matrix(
-            mass_parallelepiped(
-                hand_mass, 
-                (hand_length/10, hand_length, hand_length/10)),
-                Hg.transl(0, hand_length/2, 0)))
-
+        mass=arboris.massmatrix.transport(
+            arboris.massmatrix.box(
+                (hand_length/20., -hand_length/2., hand_length/20.),
+                hand_mass),
+            Hg.transl(0., -hand_length/2., 0.)))
 
     # create a joint between the ground and the arm
     shoulder = RzJoint(name=name+'Shoulder')
