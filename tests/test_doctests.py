6439a5a1ae9e01b7545d5d421c96f08632060141
Sébastien Barthélémy <barthelemy@crans.org>
2010-09-10 17:52:50 +0200

    fixed test_doctest which used to skip arboris.robots

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -1,22 +1,32 @@
 # coding=utf-8
 """Run all doctests from the arboris module."""
 
-import arboris
 import unittest
 import doctest
 import glob
 import os
 
-suite = unittest.TestSuite()
-for module_name in arboris.__all__:
-    exec('from arboris import {0} as module'.format(module_name))
-    try:
-        suite.addTest(doctest.DocTestSuite(module))
-    except ValueError as e:
-        if len(e.args)>=2 and e.args[1]=="has no tests":
+def doctest_package(suite, package_name):
+    """Recursively add the doctests from a package.
+    """
+    exec('import {0} as package'.format(package_name))
+    for module_name in package.__all__:
+        exec('from {0} import {1} as module'.format(package_name, module_name))
+        try:
+            suite.addTest(doctest.DocTestSuite(module))
+        except ValueError as e:
+            if len(e.args)>=2 and e.args[1]=="has no tests":
+                pass
+            else:
+                raise e
+        try:
+            module.__all__ # raise Attribute error if not a package
+            doctest_package(suite, package_name + '.' + module_name)
+        except AttributeError:
             pass
-        else:
-            raise e
+
+suite = unittest.TestSuite()
+doctest_package(suite, 'arboris')
 
 if __name__ == "__main__":
     unittest.TextTestRunner(verbosity=2).run(suite)
